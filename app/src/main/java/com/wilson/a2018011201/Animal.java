ffb224280c455adf9887e21472a951994507b391
C1 <c1@gjun.com>
2018-01-12 10:47:12 +0800

    Initial commit

---
+++ app/src/main/java/com/wilson/a2018011201/Animal.java
@@ -0,0 +1,12 @@
+package com.wilson.a2018011201;
+
+/**
+ * Created by Student on 2018/1/12.
+ */
+
+public class Animal {
+    public String district;
+    public String address;
+    public String tel;
+    public String opening_hours;
+}
---
+++ app/src/main/java/com/wilson/a2018011201/MainActivity.java
@@ -0,0 +1,72 @@
+package com.wilson.a2018011201;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewDebug;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.gson.Gson;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click1(View v)
+    {
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        final StringRequest request = new StringRequest("http://data.ntpc.gov.tw/od/data/api/BF90FA7E-C358-4CDA-B579-B6C84ADC96A1?$format=json",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("NET", response);
+                        Gson gson = new Gson();
+                        Animal[] houses = gson.fromJson(response, Animal[].class);
+
+                        for (Animal a : houses)
+                        {
+                            Log.d("NET", a.district + "," + a.address);
+                        }
+                        /*
+                                           try {
+                                           JSONArray array = new JSONArray(response);
+                                               int i;
+                                              for (i=0;i<array.length();i++)
+                                             {
+                                                 JSONObject obj = array.getJSONObject(i);
+                                                String str = obj.getString("district");
+                                                Log.d("NET", str);
+                                             }
+                                          }
+                                          catch (JSONException e) {
+                                             e.printStackTrace();
+                                          }
+                                         */
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(request);
+        queue.start();
+
+
+
+    }
+
+}
